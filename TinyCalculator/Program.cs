bfb7b1411fcff644cc2341536b01f0f7e5e04877
Dizdarevic Armin <A.Dizdarevic2@htlvb.at>
2026-03-27 10:59:38 +0100

    Add Potenzoperator

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,1 +1,44 @@
 ﻿Console.WriteLine("== Tiny Calculator ==");
+
+int a;
+int b;
+string op;
+Console.WriteLine("Geben sie den Operaten an (+ - / * ^");
+op = Console.ReadLine();
+
+Console.WriteLine("Geben sie die erste Zahl an");
+string number1 = Console.ReadLine();
+
+Console.WriteLine("Geben sie die zweite Zahl an");
+string number2 = Console.ReadLine();
+
+a = int.Parse(number1);
+b = int.Parse(number2);
+
+
+
+int Calculate(int a, int b, string op)
+{
+    if (op == "+")
+    {
+        return a + b;
+    }
+    else if (op == "-")
+    {
+        return a - b;
+    }
+    else if (op == "*")
+    {
+        return a * b;
+    }
+    else if (op == "/")
+    {
+        return a / b;
+    }
+    else if (op == "^")
+    {
+        return (int)Math.Pow((double)a, (double)b);
+    }
+
+    return 0;
+}
